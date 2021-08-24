3e8eba9f6e5f0ece38da66db91b667b12f411c6d
florian-ciuculescu <florian.ciuculescu@mastercard.com>
2021-08-24 07:41:23 +0100

    created project

---
+++ src/main/java/com/springframework5/spring5jokesapp/Spring5JokesAppApplication.java
@@ -0,0 +1,13 @@
+package com.springframework5.spring5jokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5JokesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5JokesAppApplication.class, args);
+    }
+
+}
